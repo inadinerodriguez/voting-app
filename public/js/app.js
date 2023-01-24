3137111345b87f7138ec0dd78222d03244370793
Nadine Rodriguez <inadinerod@gmail.com>
2023-01-23 19:06:47 -0500

    Add up voting on caret

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,14 @@
 
 class ProductList extends React.Component {
+    handleProductUpVote(productId) {
+        console.log(productId + ' was upvoted');
+    }
+
     render() {
         const products = Seed.products.sort((a,b) => (
             b.votes - a.votes
         ));
-        const productComponents = Seed.products.map((product) => (
+        const productComponents = products.map((product) => (
             <Product 
             key={"product-" + product.id}
             id={product.id}
@@ -14,6 +18,7 @@ class ProductList extends React.Component {
             votes={product.votes}
             submitterAvatarUrl={product.submitterAvatarUrl}
             productImageUrl={product.productImageUrl}
+            onVote={this.handleProductUpVote}
             />
         ));
         return (
@@ -25,6 +30,15 @@ class ProductList extends React.Component {
 } 
 
 class Product extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+
+    handleUpVote() {
+        this.props.onVote(this.props.id);
+    }
+
     render() {
         return (
             <div className="item">
@@ -33,7 +47,7 @@ class Product extends React.Component {
                 </div>
                 <div className="middle aligned content">
                     <div className="header">
-                        <a>
+                        <a onClick={this.handleUpVote}>
                             <i className="large caret up icon" />
                         </a>
                         {this.props.votes}
